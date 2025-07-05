ac6fa1113710d73eb3bca155c963b75223b14241
iamrealashar <asharjehangir786@gmail.com>
2025-07-05 15:45:39 +0500

    Add animated overlay intro and update history section
    Introduced a bar overlay animation using GSAP for the main page and added corresponding HTML and CSS. Updated z-index values for header and nav for proper stacking. Enhanced test.html with a new Greek history section featuring background imagery, improved typography, and GSAP scroll-triggered animations.

--- gsap.js
+++ gsap.js
@@ -1,38 +1,42 @@
 document.addEventListener("DOMContentLoaded", () => {
-let intro = gsap.timeline({});
+  let intro = gsap.timeline({});
 
-intro.from('header', {
-  duration: 1.5,
-  opacity: 0,
-  y: "-100vh",
-  ease: 'power2.out'
-},"first")
-.from('#home img', {
-    duration: 1.5,
+  intro.to(".bar", {
+      height: 0,
+      stagger: 0.1,
+      duration: 1.5,
+      ease: "power2.inOut",
+    })
+  .from('header', {
+    duration: 1,
     opacity: 0,
-    scale: 0.5,
+    y: "-100vh",
     ease: 'power2.out'
-  },"first")
-.from('#home h1', {
-  duration: 1.5,
-  y: "100vh",
-  ease: 'power2.out'
-})
-.from('#home p', {
-  duration: 1,
-  opacity: 0,
-  y: "100%",
-  ease: 'power2.out',
-letterSpacing: "0rem",
-wordSpacing: "0rem",
-})
-.from('.scroll', {
-    duration: 2,
-    delay: 0.5,
-    opacity: 0,
-    y: "100vh",
-    ease: 'power2.out'
-    },"first")
-
-
+  }, "first")
+    .from('#home img', {
+      duration: 1,
+      y: "100vh",
+      scale: 0.5,
+      ease: 'power2.out'
+    }, "first")
+    .from('#home h1', {
+      duration: 1.5,
+      y: "100vh",
+      ease: 'power2.out'
+    })
+    .from('#home p', {
+      duration: 1,
+      opacity: 0,
+      y: "100%",
+      ease: 'power2.out',
+      letterSpacing: "0rem",
+      wordSpacing: "0rem",
+    })
+    .from('.scroll', {
+      duration: 2,
+      delay: 0.5,
+      opacity: 0,
+      y: "100vh",
+      ease: 'power2.out',
+    }, "first")
 });
